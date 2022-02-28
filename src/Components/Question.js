7e93f5c3a6abe4ab144eb0696fe99a7e19e4a9ae
Selim Reza <cseselimreza@gmail.com>
2022-02-28 23:27:41 +0600

    question create form done

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -18,6 +18,11 @@ function Question(){
         id : '',
         version_id: '',
         class_id:'',
+        title: '',
+        type: '',
+        question_level_id: '',
+        mark: '',
+        options:[{}],
     };
     
     /*============version list for dropdown option=============*/
@@ -35,7 +40,34 @@ function Question(){
     const validationSchema = Yup.object({
         version_id : Yup.string().required('version is required'),
         class_id : Yup.string().required('class is required'),
+        title : Yup.string().required('Title is required'),
+        type : Yup.string().required('Question Type is required'),
+        question_level_id : Yup.string().required('Question level Id is required'),
+        mark : Yup.string().required('Mark is required'),
     })
+
+    const [inputFields, setInputFields] = useState(initialValues.options);
+
+    const addInputField = ()=>{
+        setInputFields([...inputFields, {
+            fullName:'',
+        }])
+    }
+
+    const removeInputFields = (index)=>{
+        const rows = [...inputFields];
+        rows.splice(index, 1);
+        setInputFields(rows);
+   }
+
+   const handleChange = (index, evnt)=>{
+    const { name, value } = evnt.target;
+    const list = [...inputFields];
+    list[index][name] = value;
+    setInputFields(list);
+   }
+
+   console.log(inputFields);
 
     const onSubmit = async (values,onSubmitProps) => {
         
@@ -44,7 +76,7 @@ function Question(){
 
     return(
         <>
-          <div className="content_header">
+          <div className="content_header mb-4">
                 <Row className="Row">
                     <Col xs={6} md={6} className="my-auto">
                         <div className="page_title">
@@ -60,46 +92,157 @@ function Question(){
                     {formik => (
                         <Form onSubmit={formik.handleSubmit}>
 
+                        <div className="row">
+                            <div className="col">
+                                <Form.Group className="mb-3" controlId="formBasicVersion">
+                                    <select
+                                        className="form-control"
+                                        name="version_id"
+                                        {...formik.getFieldProps('version_id')}
+                                    >
+                                        <option value="" label="Select Version" />
+                                        {
+                                            versions
+                                            ?
+                                            versions.map((version, i) =>
+                                            <option value={version.id} key={i}>{version.name}</option>
+                                            )
+                                            :null
+                                        }
+                                    </select>
+                                    {formik.touched.version_id && formik.errors.version_id ? (
+                                    <div className="error" style={{color: "red"}}>{formik.errors.version_id}</div>
+                                    ) : null}
+                                </Form.Group>
+                            </div>
+                            <div className="col">
+                                <Form.Group className="mb-3" controlId="formBasicVersion">
+                                    <select
+                                        className="form-control"
+                                        name="class_id"
+                                        {...formik.getFieldProps('class_id')}
+                                    >
+                                        <option value="" label="Select Class" />
+                                        {
+                                            classes
+                                            ?
+                                            classes.map((classItem,i) =>
+                                            <option value={classItem.id} key={i}>{classItem.name}</option>
+                                            ):null
+                                        }
+                                    </select>
+                                    {formik.touched.class_id && formik.errors.class_id ? (
+                                    <div className="error" style={{color: "red"}}>{formik.errors.class_id}</div>
+                                ) : null}
+                                </Form.Group>
+                            </div>
+                        </div>
                         <Form.Group className="mb-3" controlId="formBasicVersion">
-                            <select
-                                className="form-control"
-                                name="version_id"
-                                {...formik.getFieldProps('version_id')}
-                            >
-                                <option value="" label="Select Version" />
-                                {
-                                    versions
-                                    ?
-                                    versions.map((version, i) =>
-                                    <option value={version.id} key={i}>{version.name}</option>
-                                    )
-                                    :null
-                                }
-                            </select>
-                            {formik.touched.version_id && formik.errors.version_id ? (
-                            <div className="error" style={{color: "red"}}>{formik.errors.version_id}</div>
+                            <label htmlFor="priority">Question Title:</label>
+                            <textarea className="form-control" id="title" name="title" rows="5" cols="30"
+                                placeholder="Question Title"
+                                {...formik.getFieldProps('title')}
+                            />
+                            {formik.touched.title && formik.errors.title ? (
+                            <div className="error" style={{color: "red"}}>{formik.errors.title}</div>
                         ) : null}
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicVersion">
-                            <select
-                                className="form-control"
-                                name="class_id"
-                                {...formik.getFieldProps('class_id')}
-                            >
-                                <option value="" label="Select Class" />
-                                {
-                                    classes
-                                    ?
-                                    classes.map((classItem,i) =>
-                                    <option value={classItem.id} key={i}>{classItem.name}</option>
-                                    ):null
-                                }
-                            </select>
-                            {formik.touched.class_id && formik.errors.class_id ? (
-                            <div className="error" style={{color: "red"}}>{formik.errors.class_id}</div>
-                        ) : null}
-                        </Form.Group>
+                        <div className="row">
+                            <div className="col">
+                                <Form.Group controlId="upload" className="mb-3">
+                                    <label htmlFor="priority">Subject Thumbnail:</label><br></br>
+                                    <input id="image_urls"type="file"
+                                    // onChange={(event) => {
+                                    //     const files = event.target.files[0];
+                                    //     fileUpload(files);
+                                    //   }}
+                                    multiple />
+                                    <input id="img_has"name="img_has" type="hidden"
+                                        // value={formik.values.image_url = subjectImage}
+                                    />
+                                    {/* <img style={{width: "116px",display: "block",marginTop: "12px"}} src={subjectImage} /> */}
+                                    {formik.errors.image_url ? (
+                                    <div className="error" style={{color: "red"}}>{formik.errors.image_url}</div>
+                                ) : null}
+                                </Form.Group>
+                            </div>
+                            <div className="col">
+                                <Form.Group className="mb-3" controlId="formBasicVersion">
+                                    <label htmlFor="priority">Question Mark:</label>
+                                    <input type="number" className="form-control" id="mark" name="mark"
+                                        placeholder="Question Title"
+                                        {...formik.getFieldProps('mark')}
+                                    />
+                                    {formik.touched.mark && formik.errors.mark ? (
+                                    <div className="error" style={{color: "red"}}>{formik.errors.mark}</div>
+                                    ) : null}
+                                </Form.Group>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <Form.Group controlId="formBasicVersion">
+                                <select
+                                    className="form-control"
+                                    name="type"
+                                    {...formik.getFieldProps('type')}
+                                >
+                                    <option value="" label="Select Question Type" />
+                                    <option value="1" label="MCQ" />
+                                    <option value="2" label="True/False" />
+                                </select>
+                                {formik.touched.type && formik.errors.type ? (
+                                <div className="error" style={{color: "red"}}>{formik.errors.type}</div>
+                                ) : null}
+                                </Form.Group>
+                            </div>
+                            <div className="col">
+                                <Form.Group className="mb-3" controlId="formBasicVersion">
+                                    <select
+                                            className="form-control"
+                                            name="question_level_id"
+                                            {...formik.getFieldProps('question_level_id')}
+                                        >
+                                            <option value="" label="Select Question Level" />
+                                            <option value="1" label="1" />
+                                            <option value="2" label="2" />
+                                        </select>
+                                        {formik.touched.question_level_id && formik.errors.question_level_id ? (
+                                        <div className="error" style={{color: "red"}}>{formik.errors.question_level_id}</div>
+                                    ) : null}
+                                </Form.Group>
+                            </div>
+                        </div>
+
+                        <div className="msq_option row">
+                            <div className="col-md-6">
+                            {
+                                inputFields.map((data, index)=>{
+                                    const {fullName, emailAddress, salary}= data;
+                                    return(
+                                    <div className="row my-3" key={index}>
+                                        <div className="col">
+                                            <div className="form-group">
+                                            <input type="radio" name="checked[]" id="checked"/>
+                                                <input type="text" onChange={(evnt)=>handleChange(index, evnt)} value={fullName} name="fullName" className="form-control"  placeholder="Full Name" />
+                                            </div>
+                                        </div>
+                            
+                                        <div className="col">
+                                            {(inputFields.length!==1)? <button className="btn btn-outline-danger" onClick={removeInputFields}>x</button>:''}
+                                        </div>
+                                    </div>
+                                    )
+                                })
+                            }
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-12">
+                                <p className="btn btn-outline-success " onClick={addInputField}>Add New</p>
+                                </div>
+                            </div>
+                        </div>
 
                         <Modal.Footer>
                             <Button type="submit" variant="primary">Save</Button>
